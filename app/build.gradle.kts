2296549e5e26e3c128fdc1fa2a3fecf3c7aa04c6
LingYuZhao <113756063+BeardedManZhao@users.noreply.github.com>
2026-03-16 13:01:05 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,29 +53,17 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-
     implementation("com.google.android.material:material:1.11.0")
-
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-
     implementation("androidx.work:work-runtime:2.9.1")
-
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
     implementation("org.jsoup:jsoup:1.17.2")
-
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
     implementation("io.github.beardedmanzhao:zhao-utils:1.2.20260315")
-
     testImplementation("junit:junit:4.13.2")
-
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
